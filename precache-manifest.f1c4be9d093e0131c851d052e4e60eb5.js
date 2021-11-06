7d98e9067a2e701af9bef9f6120bc190b5009f47
IhorHrytsenko <gisyshka@gmail.com>
2021-11-06 23:44:31 +0200

    Updates

---
+++ precache-manifest.f1c4be9d093e0131c851d052e4e60eb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "973cc8f358eeecd9e8ee0c8e1e76d7b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba8a71ffdbd79161613c",
+    "url": "/static/css/main.958ea9cf.chunk.css"
+  },
+  {
+    "revision": "43ebdf61a1601f0225e9",
+    "url": "/static/js/2.9fab772d.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/static/js/2.9fab772d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba8a71ffdbd79161613c",
+    "url": "/static/js/main.d8aa34f4.chunk.js"
+  },
+  {
+    "revision": "04490997782b42783bdd",
+    "url": "/static/js/runtime-main.27a78958.js"
+  }
+]);
